e0de19b8b9d2d09b091f41d1f75923112f8e809b
Avendril <nexerio7991@gmail.com>
2018-04-24 22:07:53 +0100

    Makeshift add user info screen made for demo tommorow, no idea why profile isn't working, fuck me

--- app/src/main/java/com/example/nexer/soldierappv4/add.java
+++ app/src/main/java/com/example/nexer/soldierappv4/add.java
@@ -6,8 +6,23 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class add extends Fragment {
+
+    private EditText editTextName,editTextSurname,editTextAge,editTextEmail;
+    private Button saveButton;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_menu, container, false);
@@ -15,7 +30,32 @@ public class add extends Fragment {
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
-        getActivity().setTitle("add menu");
+        getActivity().setTitle("Add Info");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        saveButton = (Button)getView().findViewById(R.id.Save);
+        editTextName = (EditText)getView().findViewById(R.id.editTextName);
+        editTextSurname = (EditText)getView().findViewById(R.id.editTextSurname);
+        editTextAge = (EditText)getView().findViewById(R.id.editTextAge);
+        editTextEmail = (EditText)getView().findViewById(R.id.editTextEmail);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                String surname = editTextSurname.getText().toString().trim();
+                String age = editTextAge.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+                userInfo userInfo = new userInfo(name,surname,age,email);
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                databaseReference.child(user.getUid()).setValue(userInfo);
+
+                Toast.makeText(getActivity(), "Data saved!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/nexer/soldierappv4/profile.java
+++ app/src/main/java/com/example/nexer/soldierappv4/profile.java
@@ -8,15 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,9 +19,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.content.ContentValues.TAG;
 
 public class profile extends Fragment {
     private static final String TAG = "ViewDatabase";
